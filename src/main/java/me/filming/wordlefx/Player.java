4821e9661fcd42146bcb3dc209f5db6ffd12ec4e
Filming <79931565+filming@users.noreply.github.com>
2023-05-15 16:22:18 -0400

    Add player model

---
+++ src/main/java/me/filming/wordlefx/Player.java
@@ -0,0 +1,29 @@
+package me.filming.wordlefx;
+
+import java.util.ArrayList;
+
+public class Player {
+    Game game;
+    int remainingGuesses;
+    ArrayList<String> playerGuesses;
+
+    public Player(Game initGame){
+        game = initGame;
+        remainingGuesses = 0;
+        playerGuesses = new ArrayList<String>();
+    }
+
+    public int getRemainingGuesses(){
+        return remainingGuesses;
+    }
+
+    public ArrayList<String> getPlayerGuesses(){
+        return playerGuesses;
+    }
+
+    public void makeGuess(String guess){
+        String response = game.checkGuess(guess);
+        playerGuesses.add(response);
+        remainingGuesses -= 1;
+    }
+}
