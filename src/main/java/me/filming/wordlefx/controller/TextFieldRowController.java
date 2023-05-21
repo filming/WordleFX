2c853e47a1625ee70834730f68b0f1e6c7f50be1
Filming <79931565+filming@users.noreply.github.com>
2023-05-21 17:04:49 -0400

    Refractor TFR controller code

--- src/main/java/me/filming/wordlefx/controller/TextFieldRowController.java
+++ src/main/java/me/filming/wordlefx/controller/TextFieldRowController.java
@@ -47,26 +47,62 @@ public class TextFieldRowController {
 
     private void initializeListeners() {
         field1.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.length() == 1 && !isInternalChange.get()) {
+            // making sure that current field contains 1 character
+            if (newValue.length() > 1) {
+                field1.setText(newValue.substring(0, 1));
+            }
+
+            // only allowing alphabetical characters as valid input
+            if (newValue.matches("[a-zA-Z]") ){
                 field1.setText(field1.getText().toUpperCase());
                 field2.requestFocus();
+            } else {
+                field1.clear();
+            }
+        });
+
+        field1.setOnKeyPressed(keyEvent -> {
+            // disable tab usage
+            if (keyEvent.getCode() == KeyCode.TAB){
+                field1.requestFocus();
+            }
+
+            // adding the ability to go to previous fields using backspace
+            if (keyEvent.getCode().isLetterKey()){
+                field2Backspace = false;
+            } else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+                if (!field2Backspace){
+                    field2Backspace = true;
+                } else {
+                    field2Backspace = false;
+                    field1.requestFocus();
+                }
             }
         });
 
         field2.textProperty().addListener((observable, oldValue, newValue) -> {
-            // making sure user only inputs 1 character
+            // making sure that current field contains 1 character
             if (newValue.length() > 1) {
                 field2.setText(newValue.substring(0, 1));
             }
 
-            if (newValue.length() == 1 && !isInternalChange.get()) {
+            // only allowing alphabetical characters as valid input
+            if (newValue.matches("[a-zA-Z]") ){
                 field2.setText(field2.getText().toUpperCase());
                 field3.requestFocus();
+            } else {
+                field2.clear();
             }
         });
 
-        // allowing better use of backspace inputs
+
         field2.setOnKeyPressed(keyEvent -> {
+            // disable tab usage
+            if (keyEvent.getCode() == KeyCode.TAB){
+                field2.requestFocus();
+            }
+
+            // adding the ability to go to previous fields using backspace
             if (keyEvent.getCode().isLetterKey()){
                 field2Backspace = false;
             } else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
@@ -80,15 +116,27 @@ public class TextFieldRowController {
         });
 
         field3.textProperty().addListener((observable, oldValue, newValue) -> {
-            // making sure user only inputs 1 character
-            if (newValue.length() == 1 && !isInternalChange.get()) {
+            // making sure that current field contains 1 character
+            if (newValue.length() > 1) {
+                field3.setText(newValue.substring(0, 1));
+            }
+
+            // only allowing alphabetical characters as valid input
+            if (newValue.matches("[a-zA-Z]") ){
                 field3.setText(field3.getText().toUpperCase());
                 field4.requestFocus();
+            } else {
+                field3.clear();
             }
         });
 
-        // allowing better use of backspace inputs
         field3.setOnKeyPressed(keyEvent -> {
+            // disable tab usage
+            if (keyEvent.getCode() == KeyCode.TAB){
+                field3.requestFocus();
+            }
+
+            // adding the ability to go to previous fields using backspace
             if (keyEvent.getCode().isLetterKey()){
                 field3Backspace = false;
             } else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
@@ -102,19 +150,27 @@ public class TextFieldRowController {
         });
 
         field4.textProperty().addListener((observable, oldValue, newValue) -> {
-            // making sure user only inputs 1 character
+            // making sure that current field contains 1 character
             if (newValue.length() > 1) {
                 field4.setText(newValue.substring(0, 1));
             }
 
-            if (newValue.length() == 1 && !isInternalChange.get()) {
+            // only allowing alphabetical characters as valid input
+            if (newValue.matches("[a-zA-Z]") ){
                 field4.setText(field4.getText().toUpperCase());
                 field5.requestFocus();
+            } else {
+                field4.clear();
             }
         });
 
-        // allowing better use of backspace inputs
         field4.setOnKeyPressed(keyEvent -> {
+            // disable tab usage
+            if (keyEvent.getCode() == KeyCode.TAB){
+                field4.requestFocus();
+            }
+
+            // adding the ability to go to previous fields using backspace
             if (keyEvent.getCode().isLetterKey()){
                 field4Backspace = false;
             } else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
@@ -128,25 +184,36 @@ public class TextFieldRowController {
         });
 
         field5.textProperty().addListener((observable, oldValue, newValue) -> {
-            // making sure user only inputs 1 character
+            // making sure that current field contains 1 character
             if (newValue.length() > 1) {
-                field5.setText(newValue.substring(0, 1));
+                field5.setText(newValue.substring(0, 1).toUpperCase());
             }
 
+            // only allowing alphabetical characters as valid input
             if (newValue.length() == 1 && !isInternalChange.get()) {
                 isInternalChange.set(true);
-                field5.setText(field5.getText().toUpperCase());
+                if (newValue.matches("[a-zA-Z]")){
+                    field5.setText(newValue.toUpperCase());
+                } else {
+                    field5.clear();
+                }
                 isInternalChange.set(false);
 
                 System.out.println("Reached the end of the row");
             }
         });
 
-        // allowing better use of backspace inputs
         field5.setOnKeyPressed(keyEvent -> {
+            // disable tab usage
+            if (keyEvent.getCode() == KeyCode.TAB){
+                field5.requestFocus();
+            }
+
+            // adding the ability to go to previous fields using backspace
             if (keyEvent.getCode().isLetterKey()){
                 field5Backspace = false;
-            } else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+            }
+            else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
                 if (!field5Backspace){
                     field5Backspace = true;
                 } else {
@@ -154,7 +221,14 @@ public class TextFieldRowController {
                     field4.requestFocus();
                 }
             }
+            else if (keyEvent.getCode() == KeyCode.ENTER){
+                if ((field1.getText().length() == 1) && (field2.getText().length() == 1) && (field3.getText().length() == 1) && (field4.getText().length() == 1) && (field5.getText().length() == 1)){
+                    System.out.println("This is a valid entry.");
+                }
+            }
         });
+
+
 
 
     }
